0eaff5b8e21a934ea9187f24b3545fbc594a49a2
Daniel-derf <danielpardinho321@gmail.com>
2024-12-20 14:35:03 -0300

    feat: resources generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
