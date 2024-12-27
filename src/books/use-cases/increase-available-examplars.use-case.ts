c2bca71afaca03c6b28bcdb4d43920de4d9b8978
Daniel-derf <danielpardinho321@gmail.com>
2024-12-27 14:45:08 -0300

    feat: increase books exemplars logic implemented

--- src/books/use-cases/increase-available-examplars.use-case.ts
+++ src/books/use-cases/increase-available-examplars.use-case.ts
@@ -3,5 +3,11 @@ import IBooksRepository from '../repositories/books.interface.repository';
 export default class IncreaseAvailableExamplarsUseCase {
   constructor(private readonly booksRepository: IBooksRepository) {}
 
-  async execute({ bookId, quantity }: { bookId: number; quantity: number }) {}
+  async execute({ bookId, quantity }: { bookId: number; quantity: number }) {
+    const book = await this.booksRepository.findById(bookId);
+
+    book.increaseAvailableExemplarsBy(quantity);
+
+    this.booksRepository.save(book);
+  }
 }
