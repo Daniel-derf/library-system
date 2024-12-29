ad87196e4ac2f2705c8a643b1ca2305fa187970b
Daniel-derf <danielpardinho321@gmail.com>
2024-12-29 15:48:16 -0300

    refactor: remove services file

---
+++ src/clients/tests/clients.controller.spec.ts
@@ -0,0 +1,10 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ClientsController } from '../clients.controller';
+
+describe('ClientsController', () => {
+  let controller: ClientsController;
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
